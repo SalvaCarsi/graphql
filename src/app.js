fe664780b413b408043b1244df047f2161d66d0a
Salva Carsi <salva.carsi@flywire.com>
2020-05-02 12:22:00 +0200

    add schema/resolver, enable ui and expose endpoint for graphiql

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
-import express from 'express';
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import schema from './schema';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  return res.send('Hello GraphQL')
+app.get("/", (req, res) => {
+  return res.send("Hello GraphQL");
 });
 
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  })
+);
+
 app.listen(PORT, () => {
-  console.log(`Server is running at PORT ${PORT}`)
+  console.log(`Server is running at PORT ${PORT}`);
 });
