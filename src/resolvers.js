58b172d57b5a82ebcaaec22e2e0abceef7591745
Salva Carsi <salva.carsi@flywire.com>
2020-05-03 11:56:24 +0200

    5. Create Record in MongoDB Using GraphQL Mutation

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,9 +1,14 @@
+import Product from './models/product';
+
 export const resolvers = {
   Query: {
-    hello(parent, args, ctx, info) {
-      console.log(ctx.user);
-      console.log(info);
-      return args.msg;
+    allProducts() {
+      return [{ _id: "kdkdkd", name: "course", qty: 1 }];
+    }
+  },
+  Mutation: {
+    async createProduct(_, {input}) {
+      return await Product.create(input);
     }
   }
 };
