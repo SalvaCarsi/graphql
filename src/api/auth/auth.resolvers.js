508203ddf95f3f566c0edcb7e8b5ab18fca03e56
Salva Carsi <salva.carsi@flywire.com>
2020-06-01 12:42:00 +0200

    13. Register and endcrypt user data and return jwt payload

---
+++ src/api/auth/auth.resolvers.js
@@ -0,0 +1,27 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { APP_SECRET } from "../../config";
+
+async function signup(_, { input }, ctx, info) {
+  const password = await bcrypt.hash(input.password, 10);
+  const user = await ctx.models.user.create({
+    email: input.email,
+    password
+  });
+  console.log(APP_SECRET);
+  const token = jwt.sign({ userId: user._id }, APP_SECRET);
+
+  return {
+    token,
+    user: {
+      _id: user._id,
+      email: user.email
+    }
+  };
+}
+
+export default {
+  Mutation: {
+    signup
+  }
+};
