be77851329d595e8f60737c115bf503640c82d3e
Salva Carsi <salva.carsi@flywire.com>
2020-06-02 18:26:13 +0200

    16. Authenticate a User using a JSON Web Token in GraphQL

--- src/api/auth/auth.resolvers.js
+++ src/api/auth/auth.resolvers.js
@@ -4,8 +4,8 @@ import { APP_SECRET } from "../../config";
 import { validateUser } from "../../utils/util";
 
 async function signup(_, { input }, ctx, info) {
-  const {value, error} = validateUser(input);
-  if(error) {
+  const { value, error } = validateUser(input);
+  if (error) {
     throw new Error(error.message);
   }
   const password = await bcrypt.hash(value.password, 10);
@@ -13,7 +13,34 @@ async function signup(_, { input }, ctx, info) {
     email: value.email,
     password
   });
-  console.log(APP_SECRET);
+  const token = jwt.sign({ userId: user._id }, APP_SECRET);
+
+  return {
+    token,
+    user: {
+      _id: user._id,
+      email: user.email
+    }
+  };
+}
+
+async function login(parent, { input }, ctx, info) {
+  const { value, error } = validateUser(input);
+  if (error) {
+    throw new Error(error.message);
+  }
+  const user = await ctx.models.user.findOne({ email: value.email });
+
+  if (!user) {
+    throw new Error("No such user");
+  }
+
+  const matched = await bcrypt.compare(value.password, user.password);
+
+  if (!matched) {
+    throw new Error("invalid password");
+  }
+
   const token = jwt.sign({ userId: user._id }, APP_SECRET);
 
   return {
@@ -27,6 +54,7 @@ async function signup(_, { input }, ctx, info) {
 
 export default {
   Mutation: {
-    signup
+    signup,
+    login
   }
 };
